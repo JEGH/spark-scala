eda84ef4e523e5a67e36ebe68d5103e510a1eef3
João Henriques <jegh33874@gmail.com>
2018-07-17 17:40:56 +0100

    Add kafka spark examples

---
+++ SparkScalaCourseProject/src/main/scala/udemy/course/sparkScala/sparkKafka/ConsumerExample.scala
@@ -0,0 +1,33 @@
+package udemy.course.sparkScala.sparkKafka
+
+import java.util
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+
+import scala.collection.JavaConverters._
+
+object ConsumerExample extends App {
+
+  import java.util.Properties
+
+  val TOPIC="test"
+
+  val  props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "something")
+
+  val consumer = new KafkaConsumer[String, String](props)
+
+  consumer.subscribe(util.Collections.singletonList(TOPIC))
+  println("entra cicloconsumer")
+  while(true){
+    val records=consumer.poll(100)
+    for (record<-records.asScala){
+      println(record)
+    }
+  }
+  println("Sai ciclo consumer")
+}
